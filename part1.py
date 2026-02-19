0088d5279fb7bf33632e11d9a4eb1308c6904749
JumpIsac321 <145168700+JumpIsac321@users.noreply.github.com>
2026-02-19 17:19:46 +0000

    part 1 done

--- part1.py
+++ part1.py
@@ -1,1 +1,12 @@
 #write your python code here
+from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
+
+ranNums = [] #name your list and make sure it is empty!
+
+
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for i in range(10): #for loop appends 5 ranNums to your list, but make sure you name your variable
+    ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
+
+
+print(ranNums) #print the list!
