e7c0f338b21d086aaaf26f17c5628be819131cbd
JumpIsac321 <145168700+JumpIsac321@users.noreply.github.com>
2026-02-19 17:38:19 +0000

    part 2b done

--- part2b.py
+++ part2b.py
@@ -12,6 +12,7 @@ for i in range(10): #for loop appends 5 ranNums to your list, but make sure you 
 print(ranNums) #print the list!
 
 random_number_to_search = randint(1, 20)
+print("Searching for number:", random_number_to_search)
 
 comparisons = 0  # Initialize the counter for comparisons
 found = False  # Variable to track if the number was found
@@ -21,3 +22,9 @@ for number in ranNums:  # Name your variable in the for loop
     if number == random_number_to_search:
         found = True  # Set found to True if the number is in the list
         break  # Exit the loop early if the number is found
+
+#change message depending on if it was found
+if found == True:
+    print("Number",random_number_to_search,"found after",comparisons,"comparisons!")
+else:
+    print("Number",random_number_to_search,"not found after",comparisons,"comparisons!")
